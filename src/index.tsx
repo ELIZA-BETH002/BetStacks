a7686bc0529dc3b8178f46a67432165389a9f549
ELIZA-BETH001 <elizabethnwaugo@gmail.com>
2025-12-30 07:40:45 +0100

    feat: export Toast

--- src/index.tsx
+++ src/index.tsx
@@ -5,3 +5,4 @@ export * from './components/Button';
 export * from './components/Card';
 export * from './components/Input';
 export * from './components/Modal';
+export * from './components/Toast';
