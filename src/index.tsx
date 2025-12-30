f4bf0e15c254369518d6e198b915a9f8e08cd5e4
ELIZA-BETH001 <elizabethnwaugo@gmail.com>
2025-12-30 07:39:18 +0100

    feat: export Card

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,4 @@ export * from './components/Header';
 export * from './components/Footer';
 export * from './components/Sidebar';
 export * from './components/Button';
+export * from './components/Card';
